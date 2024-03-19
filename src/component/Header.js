bb3f455b30b34a360be397ac636fcbcf33dd2ec8
Nilu patel <nilusingh945164@gmail.com>
2024-03-19 17:45:22 +0530

    npm

--- src/component/Header.js
+++ src/component/Header.js
@@ -2,6 +2,8 @@ import React from 'react'
 import AddIcon from '@mui/icons-material/Add';
  import {Button} from'@mui/material';
 const Header = () => {
+
+  
   return (
     <div className='text-5x1  flex justify-between text-red-500 font-bold p-3 items-center border-b-2 border-gray-500'>
    <span> Filmy <span className='text-white'>Verse</span></span>
