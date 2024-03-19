c643e8bdfb386cb0acb182c596c9447b79aa0e91
Nilu patel <nilusingh945164@gmail.com>
2024-03-19 19:26:07 +0530

    film

--- src/component/Card.js
+++ src/component/Card.js
@@ -1,21 +1,70 @@
 import React, { useState } from 'react'
+import ReactStars from 'react-stars'
 
 const Card = () => {
 
-  const [data, setdata] = useState([]);
+  const [data, setdata] = useState([
+    {
+      name: "Avanger Endgame",
+      year: "2019",
+      rating: 5,
+      img: "https://www.movieposters.com/cdn/shop/products/108b520c55e3c9760f77a06110d6a73b_240x360_crop_center.progressive.jpg?v=1573652543"
+    },
+    {
+      name: "Avanger Endgame",
+      year: "2019",
+      rating: 1,
+      img: "https://www.movieposters.com/cdn/shop/products/108b520c55e3c9760f77a06110d6a73b_240x360_crop_center.progressive.jpg?v=1573652543"
+    },
+    {
+      name: "Avanger Endgame",
+      year: "2019",
+      rating: 3.5,
+      img: "https://www.movieposters.com/cdn/shop/products/108b520c55e3c9760f77a06110d6a73b_240x360_crop_center.progressive.jpg?v=1573652543"
+    },
+    {
+      name: "Avanger Endgame",
+      year: "2019",
+      rating: 5,
+      img: "https://www.movieposters.com/cdn/shop/products/108b520c55e3c9760f77a06110d6a73b_240x360_crop_center.progressive.jpg?v=1573652543"
+    },
+    {
+      name: "Avanger Endgame",
+      year: "2019",
+      rating: 5,
+      img: "https://www.movieposters.com/cdn/shop/products/108b520c55e3c9760f77a06110d6a73b_240x360_crop_center.progressive.jpg?v=1573652543"
+    },
+    {
+      name: "Avanger Endgame",
+      year: "2019",
+      rating: 5,
+      img: "https://www.movieposters.com/cdn/shop/products/108b520c55e3c9760f77a06110d6a73b_240x360_crop_center.progressive.jpg?v=1573652543"
+    },
+    {
+      name: "Avanger Endgame",
+      year: "2019",
+      rating: 5,
+      img: "https://www.movieposters.com/cdn/shop/products/108b520c55e3c9760f77a06110d6a73b_240x360_crop_center.progressive.jpg?v=1573652543"
+    },
+    
+
+  ]);
   return (
     <div className='flex flex-wrap justify-between p-3 mt-2'>
-    
+
       {data.map((e, i) => {
         return (
-          <div className="card  font-medium  shadow-lg p-2 hover:-translate-y-2 cursor-pointer  mt-3">
-            <img className='h-50' src="https://www.movieposters.com/cdn/shop/products/108b520c55e3c9760f77a06110d6a73b_240x360_crop_center.progressive.jpg?v=1573652543" alt="Avanger" />
-            <h1><span className='text-blue-500'>Name :</span> Avanger Endgame</h1>
-            <h1><span className='text-blue-500'>Rating :</span> *</h1>
-            <h1><span className='text-blue-500'>Year:</span> 2019</h1>
+          <div  key ={i}  className="card  font-medium  shadow-lg p-2 hover:-translate-y-3 cursor-pointer
+            mt-6 transition-all duration-500">
+            <img className='h-50' src={e.img} />
+            <h1><span className='text-blue-500'>Name :</span> {e.name}</h1>
+            <h1><span className='text-blue-500'>Rating :</span> 
+            <ReactStars size={20} half={true} value={e.rating} edit={false}/>
+            </h1>
+            <h1><span className='text-blue-500'>Year:</span> {e.year}</h1>
           </div>
 
-        )})}
+        ) })}
 
     </div>
   )
