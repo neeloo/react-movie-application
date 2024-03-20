c4c25ae9484001b993ca5dcebb76e685a793ab42
Nilu patel <nilusingh945164@gmail.com>
2024-03-20 08:55:29 +0530

    npm

--- src/component/Card.js
+++ src/component/Card.js
@@ -6,7 +6,7 @@ const Card = () => {
   const [data, setdata] = useState([
     {
       name: "Avanger Endgame",
-      year: "2019",
+      year: "2018",
       rating: 5,
       img: "https://www.movieposters.com/cdn/shop/products/108b520c55e3c9760f77a06110d6a73b_240x360_crop_center.progressive.jpg?v=1573652543"
     },
@@ -46,7 +46,7 @@ const Card = () => {
       rating: 5,
       img: "https://www.movieposters.com/cdn/shop/products/108b520c55e3c9760f77a06110d6a73b_240x360_crop_center.progressive.jpg?v=1573652543"
     },
-    
+
 
   ]);
   return (
@@ -54,17 +54,17 @@ const Card = () => {
 
       {data.map((e, i) => {
         return (
-          <div  key ={i}  className="card  font-medium  shadow-lg p-2 hover:-translate-y-3 cursor-pointer
+          <div key={i} className="card  font-medium  shadow-lg p-2 hover:-translate-y-3 cursor-pointer
             mt-6 transition-all duration-500">
             <img className='h-50' src={e.img} />
             <h1><span className='text-blue-500'>Name :</span> {e.name}</h1>
-            <h1><span className='text-blue-500'>Rating :</span> 
-            <ReactStars size={20} half={true} value={e.rating} edit={false}/>
-            </h1>
+            <h1 className='flex items-center '><span className='text-blue-500 mr-1'>Rating :</span>
+              <ReactStars size={20} half={true} value={e.rating} edit={false} /> </h1>
             <h1><span className='text-blue-500'>Year:</span> {e.year}</h1>
           </div>
 
-        ) })}
+        )
+      })}
 
     </div>
   )
