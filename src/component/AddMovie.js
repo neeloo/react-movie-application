8f5401d0f26355ba16e09274c2e7c6122fd06579
Nilu patel <nilusingh945164@gmail.com>
2024-03-26 20:57:27 +0530

    film

--- src/component/AddMovie.js
+++ src/component/AddMovie.js
@@ -1,35 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Addmovie = () => {
+  const [form, setform] = useState({
+    titel: "",
+    year: " ",
+    discription: " "
+  });
   return (
     <div>
       <section class="text-gray-600 body-font relative">
         <div class="container px-5 py-8 mx-auto">
           <div class="flex flex-col text-center w-full mb-4">
-            <h1 class="sm:text-3xl text-xl font-medium title-font mb-4 text-white">Add Movie</h1>
+            <h1 class="sm:text-3xl text-xl font-medium title-font mb-4 text-pink-500">Add Movie</h1>
           </div>
           <div class="lg:w-1/2 md:w-2/3 mx-auto">
             <div class="flex flex-wrap -m-2">
               <div class="p-2 w-1/2">
                 <div class="relative">
-                  <label for="name" class="leading-7 text-sm text-gray-300">Name</label>
-                  <input type="text" id="name" name="name" class="w-full bg-white  rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                  <label for="name" class="leading-7 text-sm text-gray-300">Title</label>
+                  <input type="text" id="name" name="name" value={form.titel} onChange={(e) => setform({ ...form, title: e.target.value })}
+                    class="w-full bg-white  rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                 </div>
               </div>
               <div class="p-2 w-1/2">
                 <div class="relative">
                   <label for="email" class="leading-7 text-sm text-gray-300">Year</label>
-                  <input type="email" id="email" name="email" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                  <input type="email" id="email" name="email" value={form.year} onChange={(e) => setform({ ...form, year: e.target.value })}
+                    class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                 </div>
               </div>
               <div class="p-2 w-full">
                 <div class="relative">
                   <label for="message" class="leading-7 text-sm text-gray-300">Discription</label>
-                  <textarea id="message" name="message" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+                  <textarea id="message" name="message" value={form.discription} onChange={(e) => setform({ ...form, discription: e.target.value })}
+                    class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
                 </div>
               </div>
               <div class="p-2 w-full">
-                <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Submit</button>
+                <button class="flex mx-auto text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-700 rounded text-lg">Submit</button>
               </div>
             </div>
           </div>
