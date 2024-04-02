8fade5694db88743632b64fa688c7abf8418ee72
Nilu patel <nilusingh945164@gmail.com>
2024-04-02 19:34:47 +0530

    npm

--- src/component/Review.js
+++ src/component/Review.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ReactStars from 'react-stars'
+import { reviewsRef } from '../firebase/firebase';
+import { addDoc } from 'firebase/firestore';
+import { TailSpin } from 'react-loader-spinner';
 
 const Review = () => {
-  return (
-    
+  const [rate, setrate] = useState(0);
+  const[loading,setloading]=useState(false);
 
-    <div>
-        review
+  return (
+    <div className='mt-4  py-1  border-t-2 border-gray-700 w-full'>
+      <ReactStars size={30} half={true} onChange={(e) => setrate(e)} />
+      <input placeholder="Enter Your thoughts ..." type="text" className=' w-full p-2  outline-none header' />
+      <button className='bg-green-600 w-full p-2 mt-4 flex justify-center'> {loading? <TailSpin height={20} color='white'/>:'Share'}</button>
     </div>
-    
+
   )
 }
 
