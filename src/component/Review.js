495339fdde8d515a4daf8ca671e8d2d5768fe36f
Nilu patel <nilusingh945164@gmail.com>
2024-04-04 07:11:38 +0530

    run

--- src/component/Review.js
+++ src/component/Review.js
@@ -5,11 +5,12 @@ import { addDoc } from 'firebase/firestore';
 import { TailSpin } from 'react-loader-spinner';
 
 const Review = () => {
+  
   const [rate, setrate] = useState(0);
   const[loading,setloading]=useState(false);
 
   return (
-    
+
     <div className='mt-4  py-1  border-t-2 border-gray-700 w-full'>
       <ReactStars size={30} half={true} onChange={(e) => setrate(e)} />
       <input placeholder="Enter Your thoughts ..." type="text" className=' w-full p-2  outline-none header' />
