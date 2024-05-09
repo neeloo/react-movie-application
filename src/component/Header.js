cf15ac1553508ce235e4cf536295227cead10494
Nilu patel <nilusingh945164@gmail.com>
2024-05-09 21:09:56 +0530

    ./'

--- src/component/Header.js
+++ src/component/Header.js
@@ -3,14 +3,11 @@ import AddIcon from '@mui/icons-material/Add';
 import { Button } from '@mui/material';
 import { Link } from 'react-router-dom'
 
-
-
 const Header = () => {
-
-
   return (
     <div className=' sticky z-10  header top-0 text-4xl  flex justify-between text-red-500 font-bold p-3 items-center border-b-2 border-gray-500'>
       <Link to={'/'}><span> Filmy<span className='text-white'>Verse</span></span></Link>
+      {/* <Link className="nav-link active" to="/login">Login</Link> */}
       <Link to={'/addmovie'}>
         <h1 className='text-lg flex items-center cursor-pointer' >
           <Button>
