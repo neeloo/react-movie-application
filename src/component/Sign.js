b79aeb326bdce0909d086d1c99995885ab457c9b
Nilu patel <nilusingh945164@gmail.com>
2024-05-27 21:34:04 +0530

    nj

--- src/component/Sign.js
+++ src/component/Sign.js
@@ -122,14 +122,14 @@ import swal from "sweet-alert";
 import { addDoc } from "firebase/firestore";
 import { usersRef } from "../firebase/firebase";
 import { useNavigate } from "react-router-dom";
- //import bcrypt from 'bcryptjs';
+//import bcrypt from 'bcryptjs';
 
 import bcrypt from "bcryptjs-react";
 
 const auth = getAuth(app);
 
 const Sign = () => {
-       const navigate = useNavigate();
+    const navigate = useNavigate();
     const [form, setForm] = useState({
         name: "",
         mobile: "",
@@ -168,38 +168,38 @@ const Sign = () => {
             })
     }
 
-      const verifyOTP = () => {
+    const verifyOTP = () => {
         try {
-          setLoading(true);
-          window.confirmationResult.confirm(OTP).then((result) => {
-            uploadData();
-            swal({
-              text: "Sucessfully Registered",
-              icon: "success",
-              buttons: false,
-              timer: 3000,
-            });
-            navigate('/login')
-            setLoading(false); 
-          })
+            setLoading(true);
+            window.confirmationResult.confirm(OTP).then((result) => {
+                uploadData();
+                swal({
+                    text: "Sucessfully Registered",
+                    icon: "success",
+                    buttons: false,
+                    timer: 3000,
+                });
+                navigate('/login')
+                setLoading(false);
+            })
         } catch (error) {
-          console.log(error);
+            console.log(error);
         }
-      }
+    }
 
-      const uploadData = async () => {
+    const uploadData = async () => {
         try {
-          const salt = bcrypt.genSaltSync(10);
-          var hash = bcrypt.hashSync(form.password, salt);
-          await addDoc(usersRef, {
-            name: form.name,
-            password: hash,
-            mobile: form.mobile
-          });
-        } catch(err) {
-          console.log(err);
+            const salt = bcrypt.genSaltSync(10);
+            var hash = bcrypt.hashSync(form.password, salt);
+            await addDoc(usersRef, {
+                name: form.name,
+                password: hash,
+                mobile: form.mobile
+            });
+        } catch (err) {
+            console.log(err);
         }
-      }
+    }
 
     return (
         <div className="w-full flex flex-col mt-8 items-center">
@@ -216,12 +216,12 @@ const Sign = () => {
                                 name="message"
                                 value={OTP}
                                 onChange={(e) => setOTP(e.target.value)}
-                                className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+                                className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                         </div>
                     </div>
                     <div className="p-2 w-full">
                         <button
-                             onClick={verifyOTP}
+                            onClick={verifyOTP}
                             className="flex mx-auto text-white bg-green-600 border-0 py-2 px-8 focus:outline-none hover:bg-green-700 rounded text-lg"
                         >
                             {loading ? <TailSpin height={25} color="white" /> : "Confirm OTP"}
@@ -233,27 +233,16 @@ const Sign = () => {
                 <>
                     <div className="p-2 w-full md:w-1/3">
                         <div className="relative">
-                            <label for="message" className="leading-7 text-sm text-gray-300">
-                                Name
-                            </label>
-                            <input
-                                id="message"
-                                name="message"
-                                value={form.name}
+                            <label for="message" className="leading-7 text-sm text-gray-300"> Name</label>
+                            <input id="message" name="message" value={form.name}
                                 onChange={(e) => setForm({ ...form, name: e.target.value })}
-                                className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                            />
+                                className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                         </div>
                     </div>
                     <div className="p-2 w-full md:w-1/3">
                         <div className="relative">
-                            <label for="message" className="leading-7 text-sm text-gray-300">
-                                Mobile No.
-                            </label>
-                            <input
-                                type={"number"}
-                                id="message"
-                                name="message"
+                            <label for="message" className="leading-7 text-sm text-gray-300"> Mobile No.</label>
+                            <inputn type={"number"} id="message" name="message"
                                 value={form.mobile}
                                 onChange={(e) => setForm({ ...form, mobile: e.target.value })}
                                 className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
@@ -262,13 +251,8 @@ const Sign = () => {
                     </div>
                     <div className="p-2 w-full md:w-1/3">
                         <div className="relative">
-                            <label for="message" className="leading-7 text-sm text-gray-300">
-                                Password
-                            </label>
-                            <input
-                                type={'password'}
-                                id="message"
-                                name="message"
+                            <label for="message" className="leading-7 text-sm text-gray-300">Password </label>
+                            <input type={'password'} id="message" name="message"
                                 value={form.password}
                                 onChange={(e) => setForm({ ...form, password: e.target.value })}
                                 className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
@@ -284,7 +268,7 @@ const Sign = () => {
                         </button>
                     </div>
                 </>
-                
+
             }
             <div>
                 <p>Already have an account <Link to={'/login'}><span className="text-blue-500">Login</span></Link></p>
