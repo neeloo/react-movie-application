e2ae436bed2026e0152a547656c40cf87e23a328
Nilu patel <nilusingh945164@gmail.com>
2024-05-27 22:44:41 +0530

    pp

--- src/component/Sign.js
+++ src/component/Sign.js
@@ -1,117 +1,3 @@
-// import React, { useState } from 'react'
-// import { TailSpin } from 'react-loader-spinner';
-// import { Link } from 'react-router-dom';
-// import {getAuth, RecaptchaVerifier, signInWithPhoneNumber} from 'firebase/auth';
-// import app from '../firebase/firebase'
-// import swal from 'sweet-alert'
-
-// const auth = getAuth(app);
-
-// const Sign = () => {
-//     const [form, setform] = useState({
-//         name: "",
-//         mobile: "",
-//         password: ""
-
-//     });
-//     const [loading, setloading] = useState(false);
-//     const [otp, setotp] = useState(false);
-//     const [OTP, setOTP] = useState("");
-
-//     const generateRecaptha = () => {
-//         window.recaptchaVerifier = new RecaptchaVerifier('recaptcha-container', {
-//           'size': 'invisible',
-//           'callback': (response) => {
-//             // reCAPTCHA solved, allow signInWithPhoneNumber.
-//           }
-//         }, auth);
-//       }
-
-//       const requestOtp = () => {
-//         setloading(true);
-//         generateRecaptha();
-//         let appVerifier = window.recaptchaVerifier;
-//           signInWithPhoneNumber(auth, `+91${form.mobile}`, appVerifier)
-//           .then(confirmationResult => {
-//             window.confirmationResult = confirmationResult;
-//             swal({
-//               text: "OTP Sent",
-//               icon: "success",
-//               buttons: false,
-//               timer: 3000,
-//             });
-//             setotp(true);
-//             setloading(false);
-//           })
-//           .catch((error) => {
-//             console.log(error)
-//           }
-//         )
-//     }
-
-
-//     return (
-//         <div classNameName='w-full flex flex-col mt-8 mt items-center'>
-//             {/* <h1 classNameName='text-xl font-bold'>Sign Up</h1> */}
-//             {otp ?
-//                 <>
-//                     <div classNameName="p-2 w-full md:w-1/3">
-//                         <div classNameName="relative">
-//                             <label for="email" classNameName="leading-7 text-sm text-gray-300">OTP</label>
-//                             <input id="email" name="email" value={OTP} onChange={(e) => setOTP(e.target.value)}
-//                                 classNameName="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
-//                         </div>
-//                     </div>
-//                     <div classNameName="p-2 w-full">
-//                         <button classNameName="flex mx-auto text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-700 rounded text-lg">
-//                             {loading ? <TailSpin height={25} color="white" /> : 'Confirm OTP'} </button>
-//                     </div>
-//                 </>
-//                 :
-//                 <>
-//                     <h1 classNameName='text-xl font-bold'>Sign Up</h1>
-
-//                     <div classNameName="p-2 w-full md:w-1/3">
-//                         <div classNameName="relative">
-//                             <label for="email" classNameName="leading-7 text-sm text-gray-300">Name</label>
-//                             <input type="email" id="email" name="email" value={form.name} onChange={(e) => setform({ ...form, name: e.target.value })}
-//                                 classNameName="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
-//                         </div>
-//                     </div>
-
-//                     <div classNameName="p-2 w-full md:w-1/3">
-//                         <div classNameName="relative">
-//                             <label for="email" classNameName="leading-7 text-sm text-gray-300">Mobile No.</label>
-//                             <input type={"number"} id="email" name="email" value={form.mobile} onChange={(e) => setform({ ...form, mobile: e.target.value })}
-//                                 classNameName="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
-//                         </div>
-//                     </div>
-//                     <div classNameName="p-2 w-full md:w-1/3">
-//                         <div classNameName="relative">
-//                             <label for="email" classNameName="leading-7 text-sm text-gray-300">Password</label>
-//                             <input type="email" id="email" name="email" value={form.password} onChange={(e) => setform({ ...form, password: e.target.value })}
-//                                 classNameName="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
-//                         </div>
-//                     </div>
-//                     <div classNameName="p-2 w-full">
-//                         <button  onClick={requestOtp}
-//                         classNameName="flex mx-auto text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-700 rounded text-lg">
-//                             {loading ? <TailSpin height={25} color="white" /> : 'Request OTP'} </button>
-//                     </div>
-//                 </>
-//             }
-
-//             <div>
-//                 <p>Already have account ?<Link to={'/login'}> <span classNameName='text-blue-500'>Login</span></Link></p>
-//             </div>
-//             <div id="recaptcha-container"></div>
-//         </div>
-//     )
-// }
-
-// export default Sign;
-
-
 
 import React, { useState } from "react";
 import { TailSpin } from "react-loader-spinner";
@@ -204,7 +90,8 @@ const Sign = () => {
     return (
         <div className="w-full flex flex-col mt-8 items-center">
             <h1 className="text-xl font-bold">Sign up</h1>
-            {otpSent ?
+            {
+                /* {otpSent ?
                 <>
                     <div className="p-2 w-full md:w-1/3">
                         <div className="relative">
@@ -222,14 +109,13 @@ const Sign = () => {
                     <div className="p-2 w-full">
                         <button
                             onClick={verifyOTP}
-                            className="flex mx-auto text-white bg-green-600 border-0 py-2 px-8 focus:outline-none hover:bg-green-700 rounded text-lg"
-                        >
+                            className="flex mx-auto text-white bg-green-600 border-0 py-2 px-8 focus:outline-none hover:bg-green-700 rounded text-lg" >
                             {loading ? <TailSpin height={25} color="white" /> : "Confirm OTP"}
                         </button>
                     </div>
 
                 </>
-                :
+                : }*/
                 <>
                     <div className="p-2 w-full md:w-1/3">
                         <div className="relative">
@@ -241,8 +127,13 @@ const Sign = () => {
                     </div>
                     <div className="p-2 w-full md:w-1/3">
                         <div className="relative">
-                            <label for="message" className="leading-7 text-sm text-gray-300"> Mobile No.</label>
-                            <inputn type={"number"} id="message" name="message"
+                            <label for="message" className="leading-7 text-sm text-gray-300">
+                                Mobile No.
+                            </label>
+                            <input
+                                type={"number"}
+                                id="message"
+                                name="message"
                                 value={form.mobile}
                                 onChange={(e) => setForm({ ...form, mobile: e.target.value })}
                                 className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
@@ -251,8 +142,13 @@ const Sign = () => {
                     </div>
                     <div className="p-2 w-full md:w-1/3">
                         <div className="relative">
-                            <label for="message" className="leading-7 text-sm text-gray-300">Password </label>
-                            <input type={'password'} id="message" name="message"
+                            <label for="message" className="leading-7 text-sm text-gray-300">
+                                Password
+                            </label>
+                            <input
+                                type={'password'}
+                                id="message"
+                                name="message"
                                 value={form.password}
                                 onChange={(e) => setForm({ ...form, password: e.target.value })}
                                 className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
@@ -264,7 +160,7 @@ const Sign = () => {
                             onClick={requestOtp}
                             className="flex mx-auto text-white bg-green-600 border-0 py-2 px-8 focus:outline-none hover:bg-green-700 rounded text-lg"
                         >
-                            {loading ? <TailSpin height={25} color="white" /> : "Request OTP"}
+                            {loading ? <TailSpin height={25} color="white" /> : "Sign up"}
                         </button>
                     </div>
                 </>
